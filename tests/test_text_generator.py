893b1a581d28ab3acb937b9c1ba206fb971e950a
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-08-02 01:07:13 -0300

    Configurado github actions

--- tests/test_text_generator.py
+++ tests/test_text_generator.py
@@ -2,7 +2,9 @@ from textgenerator import text_generator
 
 
 def test_should_return_text_based_on_word_lengths_and_word_generator() -> None:
+    def word_generator(characters: int) -> str:
+        return "x"*characters
+    
     word_lengths = [2, 3]
-    word_generator = lambda characters: "x"*characters
     result: str = text_generator(word_lengths, word_generator)
     assert result == "xx xxx"
