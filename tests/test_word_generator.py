eafd187c8e318cf8c252def3af9429e7d2a21f81
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-08-02 01:13:44 -0300

    Apenas para testar actions

--- tests/test_word_generator.py
+++ tests/test_word_generator.py
@@ -11,3 +11,7 @@ def test_should_generate_word_with_exact_character_length() -> None:
     characters = 5
     result: str = word_generator(characters)
     assert len(result) == characters
+
+
+def test_actions() -> None:
+    assert True
