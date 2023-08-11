dd75222aa0df40000fa7d68c65333916d6d56ea9
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-08-11 20:12:33 -0300

    Adicionada nova interface. Adaptado o WordGenerator

--- src/textgenerator/__init__.py
+++ src/textgenerator/__init__.py
@@ -6,17 +6,18 @@ import random
 
 
 WordGenerator = Callable[[int], str]
+LetterGenerator = Callable[[], str]
 
 
 def text_generator(word_lengths: Sequence[int], word_generator: WordGenerator) -> str:
     return ' '.join(map(word_generator, word_lengths))
 
 
-def word_generator(characters: int, letter_generator: WordGenerator = lambda _: "a") -> str:
-    return ''.join([letter_generator(1) for _ in range(characters)])
+def word_generator(characters: int, letter_generator: LetterGenerator = lambda: "a") -> str:
+    return ''.join([letter_generator() for _ in range(characters)])
 
 
-def random_digit(_: int) -> str:
+def random_digit() -> str:
     return str(random.randint(0, 9))
 
 
