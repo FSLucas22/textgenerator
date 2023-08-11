5d9b2d3c44fdda0dda482d1246a4c6d506955d4f
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-08-11 20:25:06 -0300

    adicionados testes

--- tests/test_word_generator.py
+++ tests/test_word_generator.py
@@ -5,7 +5,18 @@ def letter_a() -> str:
     return "a"
 
 
-def test_should_generate_single_word_with_correct_length() -> None:
-    characters = 5
-    result: str = word_generator(characters, letter_a)
-    assert result == "a" * characters
+def test_should_generate_single_word() -> None:
+    result: str = word_generator(5)
+    assert len(result.split())
+
+
+def test_should_generate_word_with_specified_length() -> None:
+    length = 5
+    result: str = word_generator(length)
+    assert len(result) == length
+
+
+def test_should_generate_single_word_based_on_letter_generator() -> None:
+    length = 5
+    result: str = word_generator(length, letter_a)
+    assert result == "a" * length
