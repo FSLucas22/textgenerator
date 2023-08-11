4f01761b96c4f5140feaa697127da13e19a27249
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-08-11 20:38:24 -0300

    Removido teste por testar detalhe de implementação

--- tests/test_random_letter.py
+++
@@ -1,8 +0,0 @@
-from textgenerator import random_letter
-
-
-def test_should_only_return_letters() -> None:
-    for _ in range(10):
-        letter = random_letter()
-        assert len(letter) == 1
-        assert letter.isalpha()
