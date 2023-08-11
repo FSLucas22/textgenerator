7c6a3a8ff0ce8c11eeae49e7a8038e84f6f9d4c1
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-08-11 20:48:38 -0300

    Removidos testes não deterministicos

--- tests/test_word_generator.py
+++ tests/test_word_generator.py
@@ -5,17 +5,6 @@ def letter_a() -> str:
     return "a"
 
 
-def test_should_generate_single_word() -> None:
-    result: str = word_generator(5)
-    assert len(result.split())
-
-
-def test_should_generate_word_with_specified_length() -> None:
-    length = 5
-    result: str = word_generator(length)
-    assert len(result) == length
-
-
 def test_should_generate_single_word_based_on_letter_generator() -> None:
     length = 5
     result: str = word_generator(length, letter_a)
